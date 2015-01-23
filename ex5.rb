9bd66c076c356bba62251e6940d6279fda823b20
Basharat Wani <basharatw@gmail.com>
2015-01-23 11:09:50 -0500

    Ruby Projects
    Ruby 2.1.5

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+yes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{yes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+ puts "If I add Age: #{age}, Height: #{height}, and Weight: #{weight} I get #{age + height + weight}."
+sleep{5}
---
+++ ex9.rb
@@ -0,0 +1,31 @@
+#Exercise = 9
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+puts "Here are the days: #{days}"
+print "\n"
+puts "Here are the months: #{months}"
+
+print "\n"
+
+print "Here are the days: #{days}\n"
+puts ""
+print "Here are the months: #{months}"
+
+print "\n"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+print "\n"
+print  "
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"
