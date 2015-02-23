a66c11c1650fa8e49f4d855d642807465e7ccb9e
Basharat Wani <basharatw@gmail.com>
2015-02-23 16:32:45 -0500

    DB schema created and fixed a bunch of issues
    Ruby Project

--- projects/workspace/sample_app/test/models/user_test.rb
+++ projects/workspace/sample_app/test/models/user_test.rb
@@ -3,8 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
+    #@user = User.new(name: "Example User", email: "user@example.com")
+
+	@user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end 
 
   test "should be valid" do
     assert @user.valid?
@@ -47,12 +50,17 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-  
   test "email addresses should be unique" do
     duplicate_user = @user.dup
 	duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  #Enforce length is > 5 characters 
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
   
