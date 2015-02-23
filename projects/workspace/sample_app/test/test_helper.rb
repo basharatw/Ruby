9a4acf9642d5ff336536ce03aaa3bc967cffd508
Basharat Wani <basharatw@gmail.com>
2015-02-23 11:07:47 -0500

    Fixed the broken unit tests
    Ruby Project

--- projects/workspace/sample_app/test/test_helper.rb
+++ projects/workspace/sample_app/test/test_helper.rb
@@ -3,13 +3,13 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+ActiveRecord::Migration.maintain_test_schema!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
   fixtures :all
   include ApplicationHelper
-
   # Add more helper methods to be used by all tests here...
 end
 
